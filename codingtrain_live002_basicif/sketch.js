2ffd9fe51fd47226f8b2e21af28933bda111e546
d.entgelmeier <d.entgelmeier@ceyoniq.com>
2018-10-22 16:47:17 +0200

    Added script for 'Basic If with p5.js'

---
+++ codingtrain_live002_basicif/sketch.js
@@ -0,0 +1,27 @@
+// Example code from
+// Coding Train "Live Stream #2: p5.js Conditionals and Loops"
+// https://youtu.be/uEVJRE_6v0A
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    stroke(255);
+    noFill();
+    // set the fill dependent on the x coordinate
+    // of the mouse pointer
+    if (mouseX > width / 2) {
+        fill(188, 0, 225);
+    }
+    // draw a rectangle if the mouse is in the bottom half
+    if (mouseY > height / 2) {
+        rectMode(CENTER);
+        rect(300, 200, 32, 32);
+    }
+    // draw an ellipse if the mouse is in the upper half
+    if (mouseY <= height / 2) {
+        ellipse(300, 200, 32, 32);
+    }
+}
