7715a8836b1006185bb405ad8124e2f132de69a1
d.entgelmeier <d.entgelmeier@ceyoniq.com>
2018-10-23 17:17:11 +0200

    Added script for 'bouncing ball'

---
+++ codingtrain_live002_bouncingball/sketch.js
@@ -0,0 +1,28 @@
+// Example code from
+// Coding Train "Live Stream #2: p5.js Conditionals and Loops"
+// https://youtu.be/uEVJRE_6v0A
+
+var x = 0;
+var d = 3;
+var r = 100;
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+
+    // draw ball
+    ellipse(x, 200, r, r);
+
+    // move ball
+    x = x + d;
+
+    if (x < 0 || x > width) {
+        d = -1 * d;
+    }
+}
