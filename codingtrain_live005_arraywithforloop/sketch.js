90bd29a1b3714a4dc2e835271bb7452ef553b589
d.entgelmeier <d.entgelmeier@ceyoniq.com>
2019-01-05 19:44:00 +0100

    Use p5 color function

--- codingtrain_live005_arraywithforloop/sketch.js
+++ codingtrain_live005_arraywithforloop/sketch.js
@@ -3,22 +3,30 @@
 // https://youtu.be/8v_D33uGUrU
 
 // the widths of the circles
-var widths = [ 14, 76, 26, 46 ];
+var widths = [];
 // the colors of the circles,
-// note: the length of the both arrays must be equal
-var colors = [ [126, 0, 134], 
-               [45, 123, 175],
-               [212, 245, 212],
-               [198, 24, 123] ];
+var colors = [];
+
+// Note: we have to wait for initializing 
+// the arrays until p5 is ready!
 
 function setup() {
-	createCanvas( 500, 400 );
+    createCanvas(500, 400);
+
+    // note: the length of the both arrays must be equal
+    widths = [14, 76, 26, 46];
+    colors = [
+        color(126, 0, 134),
+        color(45, 123, 175),
+        color(212, 245, 212),
+        color(198, 24, 123)
+    ];
 }
 
 function draw() {
     background(0);
-    for( var i=0; i<widths.length; i++ ) {
-        fill( colors[i][0], colors[i][1], colors[i][2] );
-        ellipse( i*100 + 100, 200, widths[i] );
+    for (var i = 0; i < widths.length; i++) {
+        fill(colors[i]);
+        ellipse(i * 100 + 100, 200, widths[i]);
     }
 }
