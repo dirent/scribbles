d1bc8d94099609515e58c1b4f8092081d5e71137
dirk <dirk@gmx.org>
2019-04-19 10:30:03 +0200

    Beautify test

--- codingtrain_live006_colorbubbles/sketch.js
+++ codingtrain_live006_colorbubbles/sketch.js
@@ -6,16 +6,16 @@
 var bubbles = [];
 
 function setup() {
-    createCanvas( 600, 400 );
-    
+    createCanvas(600, 400);
+
     // set up the array of bubble objects
-    for( var i=0; i<50; i++ ) {
-        bubbles[i] = new Bubble( random(width), random(height) );
+    for (var i = 0; i < 50; i++) {
+        bubbles[i] = new Bubble(random(width), random(height));
     }
 }
 
 function mousePressed() {
-    for( var i=0; i<bubbles.length; i++ ) {
+    for (var i = 0; i < bubbles.length; i++) {
         bubbles[i].clicked();
     }
 }
@@ -23,15 +23,15 @@ function mousePressed() {
 function draw() {
     background(0);
 
-    for( var i=0; i<bubbles.length; i++ ) {
+    for (var i = 0; i < bubbles.length; i++) {
         bubbles[i].display();
         bubbles[i].move();
     }
 
     // remove bubbles that leaves the window
-    for( var i=bubbles.length-1; i>=0; i-- ) {
-        if( bubbles[i].hasLeftWindow() ) {
-            bubbles.splice(i,1);
+    for (var i = bubbles.length - 1; i >= 0; i--) {
+        if (bubbles[i].hasLeftWindow()) {
+            bubbles.splice(i, 1);
         }
     }
 
