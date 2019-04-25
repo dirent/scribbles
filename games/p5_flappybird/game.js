07ad2c4fe0e32fe20f5fef8954d809beb9a0c8f3
dirk <dirk@gmx.org>
2019-04-25 21:00:04 +0200

    Added key handling for game status

--- games/p5_flappybird/game.js
+++ games/p5_flappybird/game.js
@@ -1,6 +1,8 @@
 // A simple Flappy Bird game
 
 var score;
+// difficulty
+var pipeSpeed = 4;
 
 function setup() {
     createCanvas(800, 600);
@@ -9,6 +11,30 @@ function setup() {
 }
 
 function draw() {
-    background( 0, 136, 255 );
+    background(0, 136, 255);
     score.show();
+}
+
+function keyPressed() {
+    if (score.isReady()) {
+        // choose difficulty between 1 and 9
+        if (keyCode >= 49 && keyCode <= 57) {
+            score.start();
+            pipeSpeed = keyCode - 48;
+        }
+    } else if (score.isRunning()) {
+        // jump
+        if (keyCode === 32 || keyCode === UP_ARROW) {
+            // jump
+        }
+        // finish
+        if (keyCode === ESCAPE) {
+            score.gameOver();
+        }
+    } else if (score.isGameOver()) {
+        // restart
+        if (keyCode === ESCAPE) {
+            score.reset();
+        }
+    }
 }
--- games/p5_flappybird/score.js
+++ games/p5_flappybird/score.js
@@ -4,11 +4,20 @@ function Score(welcome) {
     this.currentScore = 0;
     this.highScore = 0;
 
-    this.isReadyToStart = function () {
-        return this.gameStatus == 0;
+    this.isReady = function () {
+        return this.gameStatus === 0;
+    }
+
+    this.isRunning = function () {
+        return this.gameStatus === 1;
+    }
+
+    this.isGameOver = function () {
+        return this.gameStatus === 2;
     }
 
     this.show = function () {
+        var scoreText = this.welcome;
         textFont("Verdana");
         textSize(height / 10);
         textAlign(CENTER);
@@ -17,8 +26,28 @@ function Score(welcome) {
         stroke(0);
         strokeWeight(10);
 
-        if (this.isReadyToStart()) {
-            text(this.welcome, 0, height / 10, width, height);
+        if (this.isRunning()) {
+            scoreText = this.currentScore + " / " + this.highScore;
         }
+        if (this.isGameOver()) {
+            scoreText = this.currentScore + " / " + this.highScore + "\n\nGAME OVER";
+        }
+        text(scoreText, 0, height / 10, width, height);
+    }
+
+    this.start = function () {
+        this.gameStatus = 1;
+        this.currentScore = 0;
+    }
+
+    this.gameOver = function () {
+        this.gameStatus = 2;
+        if (this.currentScore > this.highScore) {
+            this.highScore = this.currentScore;
+        }
+    }
+
+    this.reset = function () {
+        this.gameStatus = 0;
     }
 }
