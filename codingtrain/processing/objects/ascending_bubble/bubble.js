5ab028a2f1a06ff349adae7cf969f46d8a204cac
dirk <dirk@gmx.org>
2019-07-03 20:22:38 +0200

    Added a second bubble with different radius

--- codingtrain/processing/objects/ascending_bubble/bubble.js
+++ codingtrain/processing/objects/ascending_bubble/bubble.js
@@ -1,17 +1,18 @@
 function Bubble() {
 
-    this.x = width / 2;
+    this.x = random(width);
     this.y = height;
+    this.r = random(50, 150);
 
     this.draw = function () {
         stroke(255);
         fill(128);
-        ellipse(this.x, this.y, 36, 36);
+        ellipse(this.x, this.y, this.r, this.r);
     }
 
     this.ascend = function () {
         this.y--;
-        this.y = max(18, this.y);
+        this.y = max(this.r/2, this.y);
         this.x += random(-2, 2);
     }
 }
--- codingtrain/processing/objects/ascending_bubble/sketch.js
+++ codingtrain/processing/objects/ascending_bubble/sketch.js
@@ -1,16 +1,22 @@
 // Example code from
 // Coding Train "8.3: Defining a Class Part II - Processing Tutorial"
 // https://youtu.be/XwfOVFelLoo
+// 
+// Coding Train "8.4: Constructor Arguments - Processing Tutorial"
+// https://youtu.be/NrwaKOsplZk
 
-var b;
+var b1,b2;
 
 function setup() {
     createCanvas(600, 400);
-    b = new Bubble();
+    b1 = new Bubble();
+    b2 = new Bubble();
 }
 
 function draw() {
     background(0);
-    b.draw();
-    b.ascend();
+    b1.draw();
+    b2.draw();
+    b1.ascend();
+    b2.ascend();
 }
