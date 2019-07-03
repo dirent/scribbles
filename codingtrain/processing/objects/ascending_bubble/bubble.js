2ff909dad28b0e21a17804b0aedb460cdc029c06
dirk <dirk@gmx.org>
2019-07-03 20:36:32 +0200

    Add third bubble and overlap effect

--- codingtrain/processing/objects/ascending_bubble/bubble.js
+++ codingtrain/processing/objects/ascending_bubble/bubble.js
@@ -2,17 +2,23 @@ function Bubble() {
 
     this.x = random(width);
     this.y = height;
-    this.r = random(50, 150);
+    this.r = random(100, 200);
 
     this.draw = function () {
         stroke(255);
-        fill(128);
+        strokeWeight(2);
+        fill(128, 128, 128, 128);
         ellipse(this.x, this.y, this.r, this.r);
     }
 
     this.ascend = function () {
         this.y--;
-        this.y = max(this.r/2, this.y);
+        this.y = max(this.r / 2, this.y);
         this.x += random(-2, 2);
+    }
+
+    this.overlaps = function (other) {
+        var d = dist(this.x, this.y, other.x, other.y);
+        return d < (this.r + other.r) / 2;
     }
 }
--- codingtrain/processing/objects/ascending_bubble/sketch.js
+++ codingtrain/processing/objects/ascending_bubble/sketch.js
@@ -4,19 +4,37 @@
 // 
 // Coding Train "8.4: Constructor Arguments - Processing Tutorial"
 // https://youtu.be/NrwaKOsplZk
+//
+// Coding Train "8.5: More on Objects - Processing Tutorial"
+// https://youtu.be/V7k5bFQbhG0
 
-var b1,b2;
+var b1, b2, b3;
 
 function setup() {
     createCanvas(600, 400);
     b1 = new Bubble();
     b2 = new Bubble();
+    b3 = new Bubble();
 }
 
 function draw() {
-    background(0);
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+    if( b1.overlaps(b2) ) {
+        red = 128;
+    }
+    if( b2.overlaps(b3) ) {
+        green = 128;
+    }
+    if( b3.overlaps(b1) ) {
+        blue = 128;
+    }
+    background(red, green, blue);
     b1.draw();
     b2.draw();
+    b3.draw();
     b1.ascend();
     b2.ascend();
+    b3.ascend();
 }
