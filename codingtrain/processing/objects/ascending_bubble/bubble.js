a85eedbff594b32eb26dca41cc5700c1756d5a33
dirk <dirk@gmx.org>
2019-08-04 19:06:03 +0200

    Added color to bubble

--- codingtrain/processing/objects/ascending_bubble/bubble.js
+++ codingtrain/processing/objects/ascending_bubble/bubble.js
@@ -3,11 +3,12 @@ function Bubble() {
     this.x = random(width);
     this.y = height;
     this.r = random(100, 200);
+    this.col = color(0);
 
     this.draw = function () {
         stroke(255);
         strokeWeight(2);
-        fill(128, 128, 128, 128);
+        fill(this.col);
         ellipse(this.x, this.y, this.r, this.r);
     }
 
@@ -20,5 +21,9 @@ function Bubble() {
     this.overlaps = function (other) {
         var d = dist(this.x, this.y, other.x, other.y);
         return d < (this.r + other.r) / 2;
+    }
+
+    this.color = function (col) {
+        this.col = col;
     }
 }
--- codingtrain/processing/objects/ascending_bubble/sketch.js
+++ codingtrain/processing/objects/ascending_bubble/sketch.js
@@ -7,12 +7,22 @@
 //
 // Coding Train "8.5: More on Objects - Processing Tutorial"
 // https://youtu.be/V7k5bFQbhG0
+//
+// Coding Train "8.6: Pass by Value vs. Pass by Reference - Processing Tutorial"
+// https://youtu.be/hNR6fsksEu8
 
 let b1, b2, b3;
+
 function init() {
     b1 = new Bubble();
     b2 = new Bubble();
     b3 = new Bubble();
+}
+
+function resetColors() {
+    b1.color(color(256, 0, 0, 128));
+    b2.color(color(0, 256, 0, 128));
+    b3.color(color(0, 0, 256, 128));
 }
 
 function setup() {
@@ -27,19 +37,17 @@ function setup() {
 }
 
 function draw() {
-    var red = 0;
-    var green = 0;
-    var blue = 0;
-    if( b1.overlaps(b2) ) {
-        red = 128;
+    resetColors();
+    if (b1.overlaps(b2)) {
+        b1.color(color(256, 256, 0, 128));
     }
-    if( b2.overlaps(b3) ) {
-        green = 128;
+    if (b2.overlaps(b3)) {
+        b2.color(color(0, 256, 256, 128));
     }
-    if( b3.overlaps(b1) ) {
-        blue = 128;
+    if (b3.overlaps(b1)) {
+        b3.color(color(256, 0, 256, 128))
     }
-    background(red, green, blue);
+    background(0);
     b1.draw();
     b2.draw();
     b3.draw();
