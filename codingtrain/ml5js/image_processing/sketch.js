b007d100d2084dce76473df0d13e27e90762e3b0
Dirk Entgelmeier <d.entgelmeier@ceyoniq.com>
2019-10-19 22:09:30 +0200

    Render puffin image

--- codingtrain/ml5js/image_processing/sketch.js
+++ codingtrain/ml5js/image_processing/sketch.js
@@ -1,5 +1,19 @@
+let mobilenet;
+let puffin;
+
+function modelReady() {
+    console.log("Model is ready.");
+}
+
+function imageReady() {
+    image(puffin, 0, 0, width, height);
+}
+
 function setup() {
     createCanvas(640, 480);
     background(0);
-    console.log( "ml5js version: " + ml5.version );
+    puffin = createImg("images/puffin.jpg", imageReady);
+    puffin.hide();
+    console.log("ml5js version: " + ml5.version);
+    mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
