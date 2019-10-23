13bd07181fb324d7ebd391497a75312b43447093
Dirk Entgelmeier <d.entgelmeier@ceyoniq.com>
2019-10-23 21:52:23 +0200

    Embed video in canvas

--- codingtrain/ml5js/webcam_image_classification/sketch.js
+++ codingtrain/ml5js/webcam_image_classification/sketch.js
@@ -9,6 +9,11 @@ function setup() {
     createCanvas(640, 480);
     background(0);
     video = createCapture(VIDEO);
+    video.hide();
     console.log("ml5js version: " + ml5.version);
     mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+}
+
+function draw() {
+    image(video, 0, 0);
 }
